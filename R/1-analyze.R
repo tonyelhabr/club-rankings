499211bb824b9464f9e9d7c8dbc473f07ffdae3a
Tony ElHabr <anthonyelhabr@gmail.com>
2023-04-01 13:37:05 -0500

    better comparing

--- R/1-analyze.R
+++ R/1-analyze.R
@@ -1,11 +1,10 @@
 library(rlang)
 library(purrr)
 library(readr)
-library(glue)
 library(dplyr)
-library(openssl)
+library(tidyr)
 
-club_rankings <- c(
+rankings <- c(
   'opta',
   'fivethirtyeight'
 ) |> 
@@ -28,7 +27,7 @@ club_rankings <- c(
       )
       
       read_csv(
-        glue('https://github.com/tonyelhabr/club-rankings/releases/download/club-rankings/{.x}-club-rankings.csv'),
+        sprintf('https://github.com/tonyelhabr/club-rankings/releases/download/club-rankings/%s-club-rankings.csv', .x),
         show_col_types = FALSE
       ) |> 
         select(!!!c(constant_cols, cols))
@@ -53,12 +52,6 @@ club_rankings <- c(
     rank
   )
 
-club_rankings$id <- ifelse(
-  is.na(club_rankings$id),
-  openssl::md5(club_rankings$team),
-  club_rankings$id
-)
-
 mapping <- read_csv('team-mapping.csv', na = '') |> 
   # filter(!is.na(id_opta)) |> ## NAs for id_opta only for Chinese Super League
   filter(league_538 != 'Chinese Super League') |> 
@@ -68,50 +61,43 @@ mapping <- read_csv('team-mapping.csv', na = '') |>
     id_opta
   )
 
-latest_club_rankings <- all_club_rankings |> 
-  filter(date == '2023-04-01') |> 
-  group_by(source) |> 
-  slice_max(updated_at, n = 1, with_ties = TRUE) |> 
-  ungroup()
+latest_rankings <- rankings |> filter(date == '2023-04-01')
 
-rankings_538 <- latest_club_rankings |> 
-  filter(source == 'fivethirtyeight') |> 
-  select(
-    date,
-    rank,
-    league,
-    team,
-    id,
-    rating
-  ) |> 
-  rename_with(~sprintf('%s_538', .x), -c(date)) |> 
-  inner_join(
-    mapping,
-    by = join_by(team_538, league_538)
-  )
-
-rankings_opta <- latest_club_rankings |> 
-  filter(source == 'opta') |> 
-  select(
-    date,
-    rank,
-    team,
-    id,
-    rank,
-    rating
-  ) |> 
-  rename_with(~sprintf('%s_opta', .x), -c(date))
-
-inner_join(
-  rankings_538,
-  rankings_opta,
-  by = join_by(date, id_opta)
+reformatted_latest_rankings <- bind_rows(
+  latest_rankings |> 
+    filter(source == 'fivethirtyeight') |> 
+    inner_join(
+      mapping,
+      by = join_by(league == league_538, team == team_538)
+    ) |> 
+    mutate(
+      id = id_opta,
+      .keep = 'unused'
+    ),
+  latest_rankings |> 
+    filter(source == 'opta') |> 
+    inner_join(
+      mapping,
+      by = join_by(id == id_opta)
+    ) |> 
+    mutate(
+      league = league_538,
+      team = team_538,
+      .keep = 'unused'
+    )
 ) |> 
-  select(
-    date,
-    rank_538,
-    rank_opta,
-    league_538,
-    team_538,
-    team_opta
+  transmute(
+    team, ## 538
+    league, ## 538
+    # id, ## opta
+    across(source, ~ifelse(.x == 'fivethirtyeight', '538', .x)),
+    rank
   )
+
+compared_latest_rankings <- reformatted_latest_rankings |> 
+  pivot_wider(
+    names_from = source,
+    values_from = rank,
+    names_prefix = 'rank_'
+  ) |> 
+  arrange(rank_538)
