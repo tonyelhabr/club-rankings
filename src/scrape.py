f71be2b40b61c5c98712740cc5d8829030e9f9bd
Tony ElHabr <anthonyelhabr@gmail.com>
2024-07-08 17:43:36 -0500

    use poetry instead of pip

--- src/scrape.py
+++ src/scrape.py
@@ -7,16 +7,12 @@ current_time = datetime.now()
 opta_df = scrape_opta_club_rankings()
 opta_df = add_timestamp_cols(opta_df, current_time)
 
-create_or_update_club_rankings_release(
-  df=opta_df,
-  file_name='opta-club-rankings.csv'
-)
+create_or_update_club_rankings_release(df=opta_df, file_name="opta-club-rankings.csv")
 
-clubelo_date_str = datetime.strftime(datetime.today(), '%Y-%m-%d')
-clubelo_url = f'http://api.clubelo.com/{clubelo_date_str}'
+clubelo_date_str = datetime.strftime(datetime.today(), "%Y-%m-%d")
+clubelo_url = f"http://api.clubelo.com/{clubelo_date_str}"
 clubelo_df = pd.read_csv(clubelo_url)
 clubelo_df = add_timestamp_cols(clubelo_df, current_time)
 create_or_update_club_rankings_release(
-  df=clubelo_df,
-  file_name='clubelo-club-rankings.csv'
+    df=clubelo_df, file_name="clubelo-club-rankings.csv"
 )
